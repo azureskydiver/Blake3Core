538a6edbb235a22ec84442cb04859008db458580
Ants <code.herder@recombulator.net>
2020-06-02 15:23:10 -0400

    Add project files.

---
+++ Blake3Core/Blake3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Blake3Core
+{
+    public class Blake3 : HashAlgorithm
+    {
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override byte[] HashFinal()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
