5ca8176df67eee1825cbd2e6ca64665b2110ef8e
Ants <code.herder@recombulator.net>
2020-06-14 00:57:50 -0400

    Make test vectors serializable to make them visible to xUnit

--- Blake3Core.Tests/TestVectors.cs
+++ Blake3Core.Tests/TestVectors.cs
@@ -13,13 +13,31 @@ using Xunit.Abstractions;
 
 namespace Blake3Core.Tests
 {
-    public class TestVector
+    public class TestVector : IXunitSerializable
     {
         public int InputLength { get; set; }
         public string Hash { get; set; }
         public string KeyedHash { get; set; }
         public string DerivedKeyHash { get; set; }
         public string Key { get; set; }
+
+        public void Deserialize(IXunitSerializationInfo info)
+        {
+            InputLength = info.GetValue<int>(nameof(InputLength));
+            Hash = info.GetValue<string>(nameof(Hash));
+            KeyedHash = info.GetValue<string>(nameof(KeyedHash));
+            DerivedKeyHash = info.GetValue<string>(nameof(DerivedKeyHash));
+            Key = info.GetValue<string>(nameof(Key));
+        }
+
+        public void Serialize(IXunitSerializationInfo info)
+        {
+            info.AddValue(nameof(InputLength), InputLength);
+            info.AddValue(nameof(Hash), Hash);
+            info.AddValue(nameof(KeyedHash), KeyedHash);
+            info.AddValue(nameof(DerivedKeyHash), DerivedKeyHash);
+            info.AddValue(nameof(Key), Key);
+        }
 
         public override string ToString()
         {
